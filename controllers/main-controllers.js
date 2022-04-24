bebfe9a48aef64d038eefbea1dfac7df6d0bbc87
Amartya Pandey <amartya.pandey@pristyncare.com>
2022-04-24 16:21:43 +0530

    starting server

--- controllers/main-controllers.js
+++ controllers/main-controllers.js
@@ -1,9 +1,9 @@
 
 exports.testFunction = async (req,res) => {
 
-    let params = req.body.test;
+    let params = req.body;
 
-    console.log(test);
+    console.log(params);
 
     return res.status(200).send({'success':"done deal!!",'status':200});
 }
