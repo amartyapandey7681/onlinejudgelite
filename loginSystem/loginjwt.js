972c3f6d56a74ad92c4e730fbced18ad9675e33c
Amartya Pandey <amartya.pandey@pristyncare.com>
2022-05-26 15:17:51 +0530

    a

---
+++ loginSystem/loginjwt.js
@@ -0,0 +1,14 @@
+let jwt = require("jsonwebtoken");
+let crypto = require("crypto");
+
+
+exports.createHashFunction=()=>{
+
+
+}
+
+exports.createRandomhashes = ()=>{
+
+    
+}
+this.createHashFunction();
